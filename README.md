d77ea8832f9536feeb6a8a11772f18e5cde979a2
Javier <hjquiroga1@gmail.com>
2022-04-16 09:36:46 -0400

    added filters and search

---
+++ README.md
@@ -0,0 +1,13 @@
+Como activar un ambiente virtual
+
+Creacion
+python3 -m venv env
+
+Activacion 
+source env/bin/activate
+
+Desactivación
+deactivate
+
+pip freeze > requirements.txt
+
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 import datetime
 
 from django.utils import timezone
@@ -6,11 +7,16 @@ from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
